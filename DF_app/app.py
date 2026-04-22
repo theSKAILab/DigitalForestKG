e24f34329f8fd363019f4edaa126892611eb872b
alegomonkey <74006840+alegomonkey@users.noreply.github.com>
2026-04-21 23:45:43 -0400

    Add dropdown to nav
    Dropdown added to nav as a jinja macro so it can be reused on the index.html page as a card.

--- DF_app/app.py
+++ DF_app/app.py
@@ -1578,6 +1578,22 @@ def feasibiltycheck():
     return jsonify({'html_map':html_map})
 '''
 
+<<<<<<< Updated upstream
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
+=======
+@app.context_processor
+def inject_tasks():
+    return {
+        "app_tasks": [
+            {"endpoint": "environmentalDataTask", "label": "Environmental Data",             "icon": "/static/images/env_icon.png",       "title": "Environmental Data"},
+            {"endpoint": "treeinventorytask",     "label": "Tree Inventory Data",            "icon": "/static/images/tree_inventory.png", "title": "Tree Inventory Data "},
+            {"endpoint": "treepreferencestask",   "label": "Tree Environmental Preferences", "icon": "/static/images/analysis.png",       "title": "Tree Environmental Preferences"},
+        ]
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=5000)
+>>>>>>> Stashed changes
